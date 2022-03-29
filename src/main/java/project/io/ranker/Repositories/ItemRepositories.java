1b873c8272a535892a8cd10aafc0846e1c501ea2
kayode ojedele <ojedele46@gmail.com>
2022-03-29 14:27:59 +0100

    models and reposss edits

--- src/main/java/project/io/ranker/Repositories/ItemRepositories.java
+++ src/main/java/project/io/ranker/Repositories/ItemRepositories.java
@@ -3,6 +3,6 @@ package project.io.ranker.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.io.ranker.models.ItemModel;
 
-public interface ItemRepositories extends JpaRepository<Long, ItemModel> {
+public interface ItemRepositories extends JpaRepository<ItemModel,Long> {
 
 }
--- src/main/java/project/io/ranker/Repositories/KollectionRepo.java
+++ src/main/java/project/io/ranker/Repositories/KollectionRepo.java
@@ -3,6 +3,6 @@ package project.io.ranker.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.io.ranker.models.KollectionModel;
 
-public interface KollectionRepo extends JpaRepository<Long, KollectionModel> {
+public interface KollectionRepo extends JpaRepository<KollectionModel, Long> {
 
 }
