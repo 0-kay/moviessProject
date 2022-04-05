23547ac5d9dcb2bcab300528db2dfb7a743d464b
Lamina Samuel <samuellamina2@gmail.com>
2022-04-05 14:34:19 +0100

    authorization added

--- src/main/java/project/io/ranker/controllers/KollectionController.java
+++ src/main/java/project/io/ranker/controllers/KollectionController.java
@@ -3,6 +3,7 @@ package project.io.ranker.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import project.io.ranker.dto.KollectionDto;
 import project.io.ranker.service.KollectionService;
@@ -17,23 +18,25 @@ public class KollectionController implements SecuredRestController{
 
     private final KollectionService kollectionService;
 
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<List<KollectionDto>> getAllKollections() {
         return ResponseEntity.ok(kollectionService.findAll());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/get/{id}")
     public ResponseEntity<KollectionDto> getKollection(@PathVariable Long id) {
         return ResponseEntity.ok(kollectionService.get(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Long> createKollection(
             @RequestBody @Valid KollectionDto kollectionDto) {
         return new ResponseEntity<>(kollectionService.create(kollectionDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> updateKollection(
         @PathVariable Long id, @RequestBody @Valid KollectionDto kollectionDto) {
         kollectionService.update(id, kollectionDto);
@@ -41,6 +44,7 @@ public class KollectionController implements SecuredRestController{
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deleteKollection (@PathVariable Long id) {
         kollectionService.delete(id);
         return ResponseEntity.ok().build();
